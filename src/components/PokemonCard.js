449d012eb03ec1ba2560d584ce56b06c044b706a
oliburt <oliburt@gmail.com>
2019-10-14 18:01:51 +0100

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Card } from 'semantic-ui-react'
 
-class PokemonCard extends React.Component {
-  render() {
-    return (
-      <Card>
-        <div>
-          <div className="image">
-            <img alt="oh no!" />
-          </div>
-          <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
-          </div>
-          <div className="extra content">
-            <span>
-              <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
-            </span>
-          </div>
+const PokemonCard = props => {
+
+  const [isFront, setIsFront] = useState(true)
+  
+
+  return (
+    <Card>
+      <div onClick={() => setIsFront(!isFront)}>
+        <div className="image">
+          <img src={isFront ? props.sprites.front : props.sprites.back} alt={props.name} />
         </div>
-      </Card>
-    )
-  }
+        <div className="content">
+          <div className="header">{props.name}</div>
+        </div>
+        <div className="extra content">
+          <span>
+            <i className="icon heartbeat red" />
+            {props.stats.find(stat => stat.name === 'hp').value}
+          </span>
+        </div>
+      </div>
+    </Card>
+  )
 }
 
 export default PokemonCard
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -3,18 +3,59 @@ import PokemonCollection from './PokemonCollection'
 import PokemonForm from './PokemonForm'
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
+import API from '../adapters/API'
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: [],
+    searchString: ''
+  }
+
+  componentDidMount() {
+    API.getAllPokemon()
+      .then(pokemon => this.setState({pokemon}))
+  }
+
+  handleSearch = (e, target) => {
+    this.setState({
+      searchString: target.value
+    })
+  }
+  
+  filterResults = () => {
+    return this.state.pokemon.filter(pkmon => pkmon.name.match(this.state.searchString))
+  }
+
+  handleNewPkmonAdd = formState => {
+    const newPkmon = {
+      name: formState.name,
+      sprites: {
+        back: formState.backUrl,
+        front: formState.frontUrl
+      },
+      stats: [{
+        name: 'hp',
+        value: formState.hp
+      }]
+    }
+    console.log(newPkmon);
+    API.postNewPkmon(newPkmon).then(pkmon => this.setState({
+      pokemon: [...this.state.pokemon, pkmon]
+    }))
+
+  }
+
   render() {
+    let pokemon = this.filterResults()
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleNewPkmonAdd={this.handleNewPkmonAdd} />
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce(this.handleSearch, 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={pokemon} />
       </div>
     )
   }
