88f97838f5228422a0083d1efc28665563e968cb
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-15 14:43:25 +0100

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,41 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  state = {
+    displayCard: "front"
+  }
+
+  flipCard = () => {
+    if (this.state.displayCard === "front") {
+      this.setState({displayCard: "back"})
+     // return this.state.displayCard // don't need to return
+    } else {
+      this.setState({displayCard: "front"})
+     // return this.state.displayCard
+    }
+  }
+
+
+  findHP = () => {
+    let hp = Object.values(this.props.stats).filter(e=> e["name"] === "hp")
+    return hp[0]["value"]
+  }
+
   render() {
+    let localHP = this.findHP()
     return (
       <Card>
-        <div>
+        <div onClick={this.flipCard}>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.props.sprites[this.state.displayCard]} alt={this.props.name} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              hp {localHP}
             </span>
           </div>
         </div>
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Form } from 'semantic-ui-react'
 
+
 class PokemonForm extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
       name: '',
@@ -13,16 +14,34 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleSubmit = () => {
+    let newPoke = {
+      name: this.state.name,
+        stats: [{"value": this.state.hp,
+                "name": "hp"}],
+        sprites: {"front": this.state.frontUrl,
+                 "back": this.state.backUrl}
+    }
+    this.props.post(newPoke)
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.handleChange}/>
+            <Form.Input fluid label="hp" placeholder="hp" name="hp"  onChange={this.handleChange}/>
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" onChange={this.handleChange}/>
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl"  onChange={this.handleChange}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
